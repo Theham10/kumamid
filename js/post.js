ad5bceadce7c1acb6b57337977800781060958e6
b-hyoung <youqlrqod@gmail.com>
2025-06-15 17:20:07 +0900

    resiznge Img

--- js/post.js
+++ js/post.js
@@ -31,18 +31,25 @@ document.addEventListener("DOMContentLoaded", () => {
       const designer = data.디자이너.find(d => d.name === postData.designerName);
       // 타이틀 및 설명 채우기
       document.title = postData.postName;
-      document.querySelector('.project-client').innerHTML = `클라이언트 : ${postData.client}`;
       if(year == '2023'){
+        document.querySelector('.project-client').innerHTML = `클라이언트 : ${postData.client}`;
         document.querySelector('.project-description').innerHTML = postData.clientDescription;
+        document.querySelector('.project-section-text').innerHTML = postData.subDescription;
       }else if(year == '2025'){
+        document.querySelector('.project-client').innerHTML = `<h2 style='color:#ffa647'>클라이언트 : ${postData.client}`;
         if (postData.clientDescription) {
-        document.querySelector('.project-client').innerHTML = `
+          document.querySelector('.project-description').innerHTML = `
           <hr style='margin-bottom:30px'>
+          <h2 style='color:white'>Concept</h2><br/>
           ${postData.clientDescription}
           <hr style='margin-top:30px'>`;
+        }
+        if(postData.subDescription){
+          document.querySelector('.project-section-text').innerHTML = `<h2>Visual Expression </h2><br>${postData.subDescription}`;
+        }
+        
       }
-      }
-      document.querySelector('.project-section-text').innerHTML = postData.subDescription;
+      
 
       // 이미지 동적 처리 (필요 시 JSON에 추가해도 좋음)
       document.querySelector('.project-main-img').src =
